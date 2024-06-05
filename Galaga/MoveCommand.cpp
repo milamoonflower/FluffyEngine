91953f4ac24d2a831389156370be45e33c1ce92c
Seher Sevik <55928191+milamoonflower@users.noreply.github.com>
2024-06-05 18:47:30 +0200

    keep player in level bounds

--- Galaga/MoveCommand.cpp
+++ Galaga/MoveCommand.cpp
@@ -3,6 +3,9 @@
 #include "CharactersManager.h"
 #include "PlayerCharacter.h"
 #include "GameObject.h"
+#include "Structs.h"
+#include <iostream>
+#include <algorithm>
 
 MoveCommand::MoveCommand(int playerIndex, glm::vec2 direction, float speed)
 	: m_PlayerIndex(playerIndex)
@@ -14,12 +17,16 @@ MoveCommand::MoveCommand(int playerIndex, glm::vec2 direction, float speed)
 
 void MoveCommand::Execute()
 {
-	if (PlayerCharacter * pPlayer{ CharactersManager::GetInstance().GetPlayer(m_PlayerIndex) })
+	if (const PlayerCharacter* pPlayer{ CharactersManager::GetInstance()->GetPlayer(m_PlayerIndex) })
 	{
-		if (Fluffy::GameObject * pPlayerGameObject{ pPlayer->GetGameObject() })
+		if (Fluffy::GameObject* pPlayerGameObject{ pPlayer->GetGameObject() })
 		{
 			glm::vec2 position{ pPlayerGameObject->GetWorldPosition() };
 			position += m_Direction * (m_Speed * Fluffy::FluffyTime::DeltaTime());
+
+			const float halfSpriteSize{ pPlayer->GetSpriteSize().x / 2.0f };
+			position.x = std::clamp(position.x, halfSpriteSize, SCREEN_SIZE.x - halfSpriteSize);
+
 			pPlayerGameObject->SetWorldPosition(position);
 		}
 	}
